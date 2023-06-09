ece39cdcc950af7fcb8bcefb96ba927c74aceb63
romiperri <romyperriluna@gmail.com>
2023-06-09 16:03:37 -0300

    proyecto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Cambiar navbar en scrollY
+/* const nav = document.getElementById("menu");
+window.addEventListener("scroll", () =>
+    window.scrollY >= 50
+        ? nav.classList.add("active-nav")
+        : nav.classList.remove("active-nav")
+);
+ */
+
